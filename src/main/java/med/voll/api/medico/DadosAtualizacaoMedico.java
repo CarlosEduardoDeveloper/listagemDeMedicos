35905ece61b640975ee4d373580464602f1ee23c
Carlos Eduardo <eduardolcmdeveloper@gmail.com>
2023-09-28 10:28:57 -0300

    criacao de pacientes e tratamento de erro

--- src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.medico;
-
-import med.voll.api.endereco.DadosEndereco;
-
-public record DadosAtualizacaoMedico(Long id, String nome, String telefone, DadosEndereco endereco) {
-}
